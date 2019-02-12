d195d70294ab4a7f565f453ca603913fe968d6e8
Prof. Manoel Gadi <manoelgadi@gmail.com>
2019-02-12 15:48:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+# Write a script named main.py and inside it import all students functions, make a simple call of the functions 1,2 and 3 with N = 5, 
+# function 4 with C = 1, D = 2 and H = 3, and function 5 and 6 with the list [1,2,3,4,5]. Finally, print all the result.
